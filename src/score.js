0ab2f297610dc323b538e9c6c7ea9de4d8f6f8d3
bnd.ev <154604304+bnd-ev0530@users.noreply.github.com>
2024-01-07 11:14:25 +0900

    modify game over

--- src/score.js
+++ src/score.js
@@ -1,58 +1,72 @@
 /* 🎰🎰🎰🎰Score🎰🎰🎰🎰 */
 let millisecond = 0;
 let points = 0;
-let highScore = 0;
+let = 0;
 let cron;
 
-function crono(){
-    pause();
-    cron = setInterval(() => { score(); }, 10);
+function crono() {
+  pause();
+  cron = setInterval(() => {
+    score();
+  }, 10);
 }
-    
+
 function pause() {
-    clearInterval(crono);
+  clearInterval(crono);
 }
-    
-function score(){
-    const score = document.getElementById('points');
-    const yourScore = document.getElementById('earned-points');
-    const HighScore = document.getElementById('highscore');
-    const NewHS = document.getElementById('newhighscore');
 
-        if (!isGameOver) {
-            if ((millisecond += 10) == 100) {
-                millisecond = 0;
-                points++;
-            } 
-            if (points % 100 === 0) {
-                score.classList.add("scoreAnimation");
-                ScoreHit.play();
+function score() {
+  const score = document.getElementById("points");
+  const yourScore = document.getElementById("earned-points");
+  const HighScore = document.getElementById("highscore");
+  const NewHS = document.getElementById("newhighscore");
 
-                ScoreHit.addEventListener("ended", function() {
-                    score.classList.remove("scoreAnimation");
-                });
-            } if (points % 500 === 0) {
-                ScoreHit.pause();
-                HigherScoreHit.play();
+  if (!isGameOver) {
+    if ((millisecond += 10) == 100) {
+      millisecond = 0;
+      points++;
+    }
+    if (points % 100 === 0) {
+      score.classList.add("scoreAnimation");
+      ScoreHit.play();
 
-                HigherScoreHit.addEventListener("ended", function() {
-                    score.classList.remove("scoreAnimation");
-                });
-            } if (points > highScore) {
-                highScore = points;
-                localStorage.setItem('highScore', highScore);
-                NewHS.classList.remove("hidden");
-            }
-        }
-    score.innerText = returnData(points)
-    yourScore.innerText = 'Score: ' + returnData(points);
-    HighScore.innerText = 'HighScore: ' + returnData(highScore);
+      ScoreHit.addEventListener("ended", function () {
+        score.classList.remove("scoreAnimation");
+      });
+    }
+    if (points % 500 === 0) {
+      ScoreHit.pause();
+      HigherScoreHit.play();
+
+      HigherScoreHit.addEventListener("ended", function () {
+        score.classList.remove("scoreAnimation");
+      });
+    }
+    if (points > highScore) {
+      highScore = points;
+      localStorage.setItem("highScore", highScore);
+      NewHS.classList.remove("hidden");
+    }
+  }
+  score.innerText = returnData(points);
+  yourScore.innerText = "Score: " + returnData(points);
+  HighScore.innerText = "HighScore: " + returnData(highScore);
 }
 
 function returnData(input) {
-    return input >= 10 ? input : `0${input}`
+  return input >= 10 ? input : `0${input}`;
 }
 
-window.onload = function() {
-    highScore = localStorage.getItem('highScore') || 0;
+function shareOnTwitter() {
+  const shareUrl = "https://twitter.com/intent/tweet";
+  const text = `🖤 ts runner 💛\n Score : ${points}\n #runningTS`;
+  const url = "https://bnd-ev0530.github.io/runningTS-2024/";
+  const fullUrl = `${shareUrl}?text=${encodeURIComponent(
+    text
+  )}&url=${encodeURIComponent(url)}`;
+  window.open(fullUrl, "_blank");
 }
+
+window.onload = function () {
+  highScore = localStorage.getItem("highScore") || 0;
+};
